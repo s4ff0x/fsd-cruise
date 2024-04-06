99db83286e0d6642a21086de331c5e0bbe917926
antonm <mendelson.inbox@gmail.com>
2024-04-06 12:25:36 +0300

    1.0.5-beta.3

--- bin.js
+++ bin.js
@@ -3,6 +3,7 @@
 const { exec } = require("child_process");
 const readline = require("readline");
 const path = require("path");
+const chalk = require("chalk");
 
 // Create readline interface for user input
 const rl = readline.createInterface({
@@ -19,7 +20,7 @@ const cruiseConfigPath = path.join(__dirname, "depcruise-config.cjs");
 process.env.TSCONFIG_PATH = tsConfigPath;
 
 // Function to execute a shell command and return a promise
-function execShellCommand(cmd) {
+function execShellCommand(cmd, highlightedMsg) {
   return new Promise((resolve, reject) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
@@ -27,8 +28,9 @@ function execShellCommand(cmd) {
         reject(error);
         return;
       }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
+      if (stdout) console.log(`stdout: ${stdout}`);
+      if (stderr) console.error(`stderr: ${stderr}`);
+      if (highlightedMsg) console.log(chalk.green.bold(highlightedMsg));
       resolve(stdout);
     });
   });
@@ -36,6 +38,7 @@ function execShellCommand(cmd) {
 
 // New function to check Graphviz installation
 function checkGraphvizInstallation() {
+  console.log(chalk.yellow.bold("Check Graphviz installation..."));
   execShellCommand("dot -V")
     .then(() => {
       // Graphviz is installed, run Dependency Cruiser directly
@@ -48,14 +51,16 @@ function checkGraphvizInstallation() {
         function (answer) {
           if (answer.toLowerCase() === "yes") {
             // Install graphviz if user said yes
-            execShellCommand("brew install graphviz")
+            execShellCommand(
+              "brew install graphviz",
+              "Graphviz installed successfully!",
+            )
               .then(() => runDependencyCruiser())
               .catch((error) =>
                 console.error(`Error installing graphviz: ${error}`),
               )
               .finally(() => {
                 rl.close(); // Make sure readline is closed
-                process.exit(0); // Exit script successfully
               });
           } else {
             // Directly run the command if user said no or provided an invalid answer
@@ -70,9 +75,8 @@ function checkGraphvizInstallation() {
 function runDependencyCruiser() {
   const command = `npx -p dependency-cruiser@16.2.4 -p typescript@5.2.2 depcruise --config ${cruiseConfigPath} --output-type archi ${srcPath} | dot -T svg | depcruise-wrap-stream-in-html > fsd-high-level-dependencies.html`;
 
-  execShellCommand(command)
+  execShellCommand(command, "Dependency cruiser finished running.")
     .then(() => {
-      console.log("Dependency cruiser finished running.");
       process.exit(0); // Exit script successfully after running
     })
     .catch((error) => {
